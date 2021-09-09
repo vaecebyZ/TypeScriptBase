af221cbf5ca35269916b3f7dd598775941147e02
Sanka✨ <vaeceby2@qq.com>
2021-09-09 11:50:41 +0800

    vue3+ts

---
+++ Vue3/vue3testproject/src/views/Home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" /><br />
+    {{ hello }}<br />
+    <button @click="add">+</button>
+    {{user.name}}
+    {{user.age}}
+    <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, ref } from "vue";
+// import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+export default defineComponent({
+  name: "Home",
+  // components: {
+  //   HelloWorld,
+  // },
+  setup() {
+    // 基本用法
+    const hello = ref(1);
+    const add = () => {
+      hello.value++;
+      user.age++;
+    };
+    const user = reactive({
+      name:'sanka',
+      age:18
+    })
+
+    return {
+      hello,
+      add,
+      user
+    };
+  },
+});
+</script>
